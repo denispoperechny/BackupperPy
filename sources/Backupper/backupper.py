533c57721896f1194ec061011f7aa497e9365891
Denis Poperechny <den_po@ukr.net>
2013-12-06 01:41:42 +0200

    upd

--- sources/Backupper/backupper.py
+++ sources/Backupper/backupper.py
@@ -1,16 +1,27 @@
 #!/usr/local/bin/python
+import sys
 from Components.Logger import Logger
 
 
 __author__ = 'Denis'
 
+def unhandledExceptionUtilizer(type, value, traceback):
+        logger = Logger('Logs')
+        logger.log("Unhandled Exception")
+        logger.log("Type:" + str(type))
+        logger.log("Value:" + str(value))
+        #TODO: log the traceback
+        print("Traceback:" + traceback)
+sys.excepthook = unhandledExceptionUtilizer
 
-def func():
-    print("hi")
-    asd = Logger('Logs')
-    asd.logSkippedObject("hey")
-    asd.logSkippedObject("hey1")
-    asd.log("Started")
+class Program(object):
+
+    __logger = None
+
+    def start(self):
+        self.__logger = Logger('Logs')
+        self.__logger.log("Starting")
+
 
 if __name__ == "__main__":
-    func()
+    Program().start()
